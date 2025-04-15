f028d8c04dab6abe31f0c6ca0a7c644f7271ba27
ebbenium2025 <presidencia@ebbenium.com>
2025-04-15 12:19:04 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <H1>Curso de Git y git hub!</H1>
+    <p> ya estoy trabajando en frontend</p>
+    <p>estoy aprendiendo git, y a fusionar ramas</p>
+
+</body>
+</html>
